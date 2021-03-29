8aa81bcc53aa2b4ef117653d25dd28211953282e
dragonsuperf <dragonsuperf@gmail.com>
2021-03-29 19:09:27 +0900

    defaulttheme styled.d.ts 추가

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,17 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    themeColor: string;
+    textColor: string;
+    fallingColor: string;
+    risingColor: string;
+    baseBackgroundColor: string;
+    weakBorder: string;
+    basicColor: string;
+    backgroundColor: string;
+    moblide: string;
+    tablet: string;
+    desktop: string;
+  }
+}
