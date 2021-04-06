225b2f951e0c145bc7c0f8831bcf4da179ca636a
dragonsuperf <dragonsuperf@gmail.com>
2021-04-07 01:24:30 +0900

    Setnry 세팅 후 axios global interceptor 적용해서 API에서 에러나는 것을 리포트하도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,31 @@ import styled, { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import { theme, darkTheme } from '@/styles/theme';
 import store from '@/store/store';
+import { Integrations } from '@sentry/tracing';
+import * as Sentry from '@sentry/react';
 import ChartSection from './components/chart/ChartSection';
 import Header from './components/header/Header';
 import SideBar from './components/sidebar/SideBar';
 import DarkModeButton from './components/button/DarkModeButton';
 
+Sentry.init({
+  dsn: 'https://d8363a5886dc401c85bcdad0a68e80b5@o565801.ingest.sentry.io/5707748',
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
+
 axios.defaults.baseURL = 'https://api.bithumb.com/public';
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    Sentry.captureException(error);
+    return Promise.reject(error);
+  },
+);
 
 const Wrapper = styled.div`
   display: flex;
